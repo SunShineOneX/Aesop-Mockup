2a6ebf91f8d4fd29ea59ec90740e9c9fa75218be
amadi5892 <66728197+amadi5892@users.noreply.github.com>
2020-07-18 08:46:17 -0400

    added text and image; image won't display currently

--- src/components/Gifts-2.js
+++ src/components/Gifts-2.js
@@ -2,8 +2,16 @@ import React from 'react';
 
 function Gifts() {
     return(
-        <div>
-            
+        <div className="container">
+            <div className="row">
+                <div className="col-sm-3">
+                    <h4>Five mythical Gift Kits</h4>
+                    <p>Crafted for gods, mortals, and muses alike, our new Gift Kit collection makes for inspired gift giving this festive session.</p>
+                </div>
+                <div className="col-sm-9">
+                    <img src="../images/Aesop-Gift-Kits-2019-20-Hybris-Range-Page-Secondary-Mid-Desktop-1690x945px.png" className="img-fluid" alt="Responsive image"></img>
+                </div>
+            </div>
         </div>
     )
 }

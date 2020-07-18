01f4d8b7d7ba12d1405440aaf00df4f52b135495
amadi5892 <66728197+amadi5892@users.noreply.github.com>
2020-07-18 14:26:12 -0400

    added carousel component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Pari.css';
 import Gifts from './components/Gifts-2';
+import Carousel from './components/Carousel-3'
 
 function App() {
   return (
     <div className="">
       <header className="">
         <Gifts />
+        <Carousel />
       </header>
     </div>
   );

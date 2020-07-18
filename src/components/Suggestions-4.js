fe74018d0e4717248b3f5a1c14cf254f28c637d0
amadi5892 <66728197+amadi5892@users.noreply.github.com>
2020-07-18 16:18:14 -0400

    added div between 'find store button' and 'big picture'

--- src/components/Suggestions-4.js
+++ src/components/Suggestions-4.js
@@ -5,15 +5,16 @@ function Suggestions() {
         <div className="container">
             <div className="carousel"></div>
             <div className="row">
-                <div className="col-sm-3">
+                <div className="col-sm-5">
                     <p id="sub-title">Architecture and Design</p>
                     <p>Spaces of distinction</p>
                 </div>
-                <div className="col-sm-9">
-                    <p>Aesop stores are informed by the history, fabric and context of their local environments, yet retain an aesthetic consistency.</p>
+                <div className="col-sm-7 find-store">
+                    <p id="sub-title" >Aesop stores are informed by the history, fabric and context of their local environments, yet retain an aesthetic consistency.</p>
                     <buton id="find-btn">Find a nearby store &nbsp; &nbsp; &nbsp; &#8594;</buton>
                 </div>
             </div>
+            <div className="psh-dwn"></div>
             <div className="row">
                 <div className="col-sm-12">
                     <img id="bleeker" src={require('../images/Aesop-Bleecker-Street-Carousel-1-Desktop-2560x1440px.jpg')} alt=""></img>

e54f3d0071db756dda8757b88369c51de1b3002c
amadi5892 <66728197+amadi5892@users.noreply.github.com>
2020-07-18 15:42:26 -0400

    Added Suggestions Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Pari.css';
 import Gifts from './components/Gifts-2';
 import Carousel from './components/Carousel-3'
+import Suggestions from './components/Suggestions-4'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <header className="">
         <Gifts />
         <Carousel />
+        <Suggestions />
       </header>
     </div>
   );

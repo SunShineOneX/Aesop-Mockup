0f95f1fc322f23afc0a67e39cc2b7770d14a8c0a
amadi5892 <66728197+amadi5892@users.noreply.github.com>
2020-07-19 00:40:50 -0400

    updated photos and text for nurturing component

--- src/components/Nurturing-formulations-7.js
+++ src/components/Nurturing-formulations-7.js
@@ -14,38 +14,38 @@ function Nurturing() {
                 <div className="col-sm-10">
                 <div className="card-deck">
                         <div id="card" className="card">
-                            <img src={require('../images/for_all_products/Aesop-Skin-In-Two-Minds-Facial-Cleanser-200mL-Large-835x962px.png')} className="card-img-top" alt=""></img>
+                            <img src={require('../images/for_all_products/Aesop-Skin-In-Two-Minds-Facial-Cleanser-200mL-Large-835x962px.png')} className="card-img-top nurt1" alt=""></img>
                             <div className="card-body">
                             <p id="sub-title" className="card-title"><strong>Parsley Seed Anti-Oxidant Serum</strong></p>
                             <p id="sub-title" className="card-text">Perfect for use in humid climates</p>
                             </div>
                         </div>
                         <div id="card" className="card">
-                            <img src={require('../images/daily_products/Aesop-Body-Redemption-Body-Scrub-180mL-large.png')} className="card-img-top scrub1" alt=""></img>
+                            <img src={require('../images/for_all_products/Aesop-Skin-Mandarin-Facial-Hydrating-Cream-60mL-large.png')} className="card-img-top nurt2" alt=""></img>
                             <div className="card-body">
-                            <p id="sub-title" className="card-title"><strong>Redemption Body Scrub</strong></p>
-                            <p id="sub-title" className="card-text">Bamboo Stem and Pumice, to polish skin</p>
+                            <p id="sub-title" className="card-title"><strong>Mandarin Facial Hydrating Cream</strong></p>
+                            <p id="sub-title" className="card-text">Rapidly absorbed, lightly hydrating</p>
                             </div>
                         </div>
                         <div id="card" className="card">
-                            <img src={require('../images/daily_products/Aesop-Body-Body-Cleansing-Slab-310g-Carton-Large-1584x962px.png')} className="card-img-top scrub2" alt=""></img>
+                            <img src={require('../images/for_all_products/Aesop-Skin-Fabulous-Face-Oil-25mL-Large-684x668px.png')} className="card-img-top nurt3" alt=""></img>
                             <div className="card-body">
-                            <p id="sub-title" className="card-title"><strong>Body Cleansing Scrub</strong></p>
-                            <p id="sub-title" className="card-text">Enriched with Bergamot Rine, Tahitian</p>
+                            <p id="sub-title" className="card-title"><strong>Fabulous Face Oil</strong></p>
+                            <p id="sub-title" className="card-text">Softens, nourishes and balances skin</p>
                             </div>
                         </div>    
                         <div id="card" className="card">
-                            <img src={require('../images/daily_products/Aesop-Body-Body-Cleansing-Slab-310g-Carton-Large-1584x962px.png')} className="card-img-top scrub2" alt=""></img>
+                            <img src={require('../images/for_all_products/Aesop-SkinParsley-Seed-Facial-Cleanser-100mL-large.png')} className="card-img-top nurt4" alt=""></img>
                             <div className="card-body">
-                            <p id="sub-title" className="card-title"><strong>Body Cleansing Scrub</strong></p>
-                            <p id="sub-title" className="card-text">Enriched with Bergamot Rine, Tahitian</p>
+                            <p id="sub-title" className="card-title"><strong>Parsley Seed Facial Cleanser</strong></p>
+                            <p id="sub-title" className="card-text">For those in polluted urban environments</p>
                             </div>
                         </div>
                         <div id="card" className="card">
-                            <img src={require('../images/daily_products/Aesop-Body-Body-Cleansing-Slab-310g-Carton-Large-1584x962px.png')} className="card-img-top scrub2" alt=""></img>
+                            <img src={require('../images/for_all_products/AesopSkin-B-Tea-Balancing-Toner-200mL-large.png')} className="card-img-top nurt5" alt=""></img>
                             <div className="card-body">
-                            <p id="sub-title" className="card-title"><strong>Body Cleansing Scrub</strong></p>
-                            <p id="sub-title" className="card-text">Enriched with Bergamot Rine, Tahitian</p>
+                            <p id="sub-title" className="card-title nurt5"><strong>B & Tea Balancing Toner</strong></p>
+                            <p id="sub-title" className="card-text">Particularly suited to sensitive skin</p>
                             </div>
                         </div>
                     </div>

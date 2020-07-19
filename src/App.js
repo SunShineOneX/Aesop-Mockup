941b89aaad3620ddaf7c62f357db76f548208ba0
SunShineOneX <SunShineOneX@gmail.com>
2020-07-19 10:40:15 -0400

    final footer updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './Jeff.css';
 import Homepage from "./components/Homepage-1";
+import Homepage2 from "./components/Homepage-2";
+import Footer from "./components/Footer-8";
 
 
 function App() {
   return (
     <div>
       <Homepage />
+      <Homepage2 />
+      <Footer />
     </div>
   );
 }
